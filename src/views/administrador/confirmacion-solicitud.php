b624444dad27939256e21d93dca11ec9aefec2bd
DANIEL FERNANDO BELTRAN HUERTAS <beltran_daniel@javeriana.edu.co>
2020-06-03 15:57:47 -0500

    Segundo método de envio de correo

--- src/views/administrador/confirmacion-solicitud.php
+++ src/views/administrador/confirmacion-solicitud.php
@@ -1,4 +1,26 @@
 <?php
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+  include_once '../../../vendor/phpmailer/phpmailer/src/Exception.php';
+  require '../../../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+  include_once '../../../vendor/phpmailer/phpmailer/src/SMTP.php';
+
+  $mail = new PHPMailer();
+
+  $mail->IsSMTP();
+
+  $mail->SMTPDebug  = 0;  
+  $mail->SMTPAuth   = TRUE;
+  $mail->SMTPSecure = "tls";
+  $mail->Port       = 587;
+  $mail->Host       = "smtp.gmail.com";
+  $mail->Username   = "akasutamales@gmail.com";
+  $mail->Password   = "ByakuganHachibi";
+  $mail->IsHTML(true);
+  
+?>
+
+<?php
 
     include_once '../../services/SolicitudService.php';
     include_once '../../services/PacienteService.php';
@@ -37,15 +59,33 @@
             . "\nUnidades asignadas: " . $resultado['asignados']
             . "\nUnidades no asignadas: " . $resultado['no_asignados']
             . "\n\nAtentamente, Akasutamales.";
-            $mensaje=wordwrap($mensaje,70,"\n");
+            //$mensaje=wordwrap($mensaje,70,"\n");
+
+            /** Método 1 */
             if(mail($medico->getEmail(),"Solicitud aprobada",$mensaje)){
                 echo "La solicitud fue aprobada correctamente, se notifico al correo " . $medico->getEmail()."<br>";
             }else{
                 echo "Error en el envío del correo<br>";
             }
+
+            
+            /** Método 2 */
+            /*$mail->AddAddress($medico->getEmail(), $medico->getNombre());
+            $mail->SetFrom("akasutamales@gmail.com", "Akasutamales");
+            $mail->Subject = "Solicitud aprobada";
+            $content = $mensaje;
+
+            $mail->MsgHTML($content); 
+            if($mail->Send()) {
+                echo "La solicitud fue aprobada correctamente, se notifico al correo " . $medico->getEmail()."<br>";
+            } else {
+                echo "Error en el envío del correo<br>";
+            }*/
         }else{
             echo "ERROR: no se pudo procesar la solicitud<br>";
         }
+
+        
     ?>
 </body>
 </html>
--- src/views/administrador/denegacion-solicitud.php
+++ src/views/administrador/denegacion-solicitud.php
@@ -1,4 +1,26 @@
 <?php
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+  include_once '../../../vendor/phpmailer/phpmailer/src/Exception.php';
+  require '../../../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+  include_once '../../../vendor/phpmailer/phpmailer/src/SMTP.php';
+
+  $mail = new PHPMailer();
+
+  $mail->IsSMTP();
+
+  $mail->SMTPDebug  = 0;  
+  $mail->SMTPAuth   = TRUE;
+  $mail->SMTPSecure = "tls";
+  $mail->Port       = 587;
+  $mail->Host       = "smtp.gmail.com";
+  $mail->Username   = "akasutamales@gmail.com";
+  $mail->Password   = "ByakuganHachibi";
+  $mail->IsHTML(true);
+  
+?>
+
+<?php
 
     include_once '../../services/SolicitudService.php';
     include_once '../../services/PacienteService.php';
@@ -28,9 +50,10 @@
 </head>
 <body>
     <?php
-        $solicitudService->rechazar($solicitud->getId());
-        if( true ){ // TODO
+        $exito = $solicitudService->rechazar($solicitud->getId());
+        if( $exito ){ 
 
+            // Método 1
             $mensaje = "La solicitud del equipo " . $equipo->getCodigo() . " fue rechazada."
             . "\n\nAtentamente, Akasutamales.";
             $mensaje=wordwrap($mensaje,70,"\n");
@@ -39,6 +62,20 @@
             }else{
                 echo "Error en el envío del correo<br>";
             }
+
+            /** Método 2 */
+            /*$mail->AddAddress($medico->getEmail(), $medico->getNombre());
+            $mail->SetFrom("akasutamales@gmail.com", "Akasutamales");
+            $mail->Subject = "Solicitud rechazada";
+            $content = $mensaje;
+
+            $mail->MsgHTML($content); 
+            if($mail->Send()) {
+                echo "La solicitud fue rechazada, se notifico al correo " . $medico->getEmail()."<br>";
+            } else {
+                echo "Error en el envío del correo<br>";
+            }*/
+
         }else{
             echo "ERROR: no se pudo procesar la solicitud<br>";
         }
